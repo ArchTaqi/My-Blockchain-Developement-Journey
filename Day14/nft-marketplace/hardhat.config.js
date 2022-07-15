d404d3aa1b38d95eabb7a67b3382d5e569593a40
Ronin <31029205+0xronin@users.noreply.github.com>
2022-07-15 17:27:29 +0530

    r2w3-7

---
+++ Day14/nft-marketplace/hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/ndsTuce1iY1YVdD9CpXdWEURKS5HFtiX",
+      accounts: [ "16f1c368d06f48825ffa44b577e1f1620628adcd1c8deab2903d211183963335" ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
